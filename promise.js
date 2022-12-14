91dcd3b9642f522254af1169f3c16c78bef037d9
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-12-13 16:40:42 -0800

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,14 +1,44 @@
-let myPromise = new Promise(function(resolve, reject) {
+/*
+  When writing a promise
+  the @param {resolve, reject} must be inside the function
+*/
+
+const myPromise = new Promise(function(resolve, reject) {
+  const number = Math.random()
   setTimeout(function() {
-    resolve();
-  }, 1000);
+    console.log(`The value is: ${number}`)
+  if(number>0.5){
+    console.log(`The result was resovlve ${number}`)
+    resolve(number)
+  }else{
+    console.log(`The result was rejected ${number}`)
+    reject(number)
+  }
+}, 1000);
 });
 
-myPromise
-  .then(function() {
-    return 99;
-  })
-  .then(function(number) {
-    console.log(number);
-  });
+myPromise.then((number)=>{
+  console.log('Success')
+}).catch((number)=>{
+  console.log('Failure');
+})
+// .then(()=>{
+//   console.log("complete");
+// })
 
+const awaitTest = async function(){
+try{
+// waits for the promise to be resolve
+const test = await myPromise
+console.log(`Await/Async succesful`)
+console.log('Complete, through resolve()')
+} 
+catch{
+// the promise was rejected() but at setTimeout() is needed to have print in order
+setTimeout(()=>{
+  console.log(`Await/Async succesful`)
+  console.log('Complete, through reject()')
+},100)
+}
+}
+awaitTest()

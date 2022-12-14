a85c96911104078fd6f8118ed1d33460a97c28cb
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-12-13 16:41:10 -0800

    Update remove-notice.js

---
+++ remove-notice.js
@@ -0,0 +1,12 @@
+const notice = document.getElementById('maintenance-notice')
+// console.log(notice)
+const removeNotice = setTimeout(()=>{
+    // all possible ways to do this
+
+    // notice.setAttribute('hidden', '')
+
+    // notice.remove()
+
+    notice.style.display ="none"
+
+},5000)

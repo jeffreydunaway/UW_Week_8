95f6695059652b21ddd2ab314f86331809b858ca
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-12-13 16:42:55 -0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,21 +1,22 @@
 // create api-key.js file with const API_KEY="your_api_key" in this same directory to use
 const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 
-const url = `${BASE_URL}?q=tech&api-key=${API_KEY}`;
+const API_KEY = '9PQ46JC0RXhkMuVMTY0AcKECLY6qpqm1'const url = `${BASE_URL}/lists/names.json?api-key=${API_KEY}`;
 
+const url = `${BASE_URL}/lists/names.json?api-key=${API_KEY}`;
+
+  use.cors({Origin: *})
 fetch(url)
   .then(function(data) {
+    console.log(data.json)
     return data.json();
   })
   .then(function(responseJson) {
     console.log(responseJson);
-
     let article = responseJson.response.docs[0];
     console.log(article);
-
     const mainHeadline = article.headline.main;
     document.getElementById('article-title').innerText = mainHeadline;
-
     if (article.multimedia.length > 0) {
       const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
       document.getElementById('article-img').src = imgUrl;
